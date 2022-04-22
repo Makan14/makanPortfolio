499aad7923651dcd0ff53643c09fba884a0a749e
Makan_14 <m.macalou522@gmail.com>
2022-04-22 09:15:21 +0200

    symfony makanPortfolio

--- src/Controller/MesProjetsController.php
+++ src/Controller/MesProjetsController.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class MesProjetsController extends AbstractController
 {
@@ -20,13 +22,34 @@ class MesProjetsController extends AbstractController
     /**
      * @Route("/admin/formprojets", name="app_form_projets")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, SluggerInterface $slugger): Response
     {
         $projets = new MesProjets(); 
         $form = $this->createForm(ProjetsType::class, $projets);   
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
+            
+            $photoProjets = $form->get('images')->getData();
+       
+            if($photoProjets){
+           $originalFilename = pathinfo($photoProjets->getClientOriginalName(), PATHINFO_FILENAME);
+           $safeFilename = $slugger->slug($originalFilename);
+           $newFilename = $safeFilename.'-'.uniqid().'.'.$photoProjets->guessExtension();
+             try {
+                $photoProjets->move(
+                    $this->getParameter('images'), 
+                    $newFilename
+                );
+             }catch (FileException $e){
+
+             }
+              $projets->setImages($newFilename);
+            }else{
+                dd("Aucun logo"); 
+            } 
+
+
             $this->manager->persist($projets); 
             $this->manager->flush(); 
             return $this->redirectToRoute('app_home'); 
@@ -59,12 +82,30 @@ class MesProjetsController extends AbstractController
     /**
      * @Route("/admin/projets/edit/{id}", name="app_admin_projets_edit")
      */
-    public function projetsEdit(MesProjets $projets, Request $request): Response 
+    public function projetsEdit(MesProjets $projets, SluggerInterface $slugger, Request $request): Response 
     {
             $formEdit = $this->createForm(ProjetsType::class, $projets);
             $formEdit->handleRequest($request);
 
             if ($formEdit->isSubmitted() && $formEdit->isValid()) {
+            $photoProjets = $formEdit->get('images')->getData();
+
+            if($photoProjets){
+                $originalFilename = pathinfo($photoProjets->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photoProjets->guessExtension();
+                  try {
+                     $photoProjets->move(
+                         $this->getParameter('images'), 
+                         $newFilename
+                     );
+                  }catch (FileException $e){
+     
+                  }
+                   $projets->setImages($newFilename);
+                 }else{
+                     dd("Aucun logo"); 
+                 } 
                 $this->manager->persist($projets);  
                 $this->manager->flush(); 
                 return $this->redirectToRoute('app_mes_projets'); 
@@ -102,6 +143,8 @@ class MesProjetsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()){
             $this->manager->persist($projets); 
             $this->manager->flush(); 
+            return $this->redirectToRoute('app_mes_projets'); 
+
 
         }
 
@@ -127,6 +170,21 @@ class MesProjetsController extends AbstractController
         return $this->render('mes_projets/gestion.html.twig', [ 
             'projets' => $allTable, 
         ]);   
+    
+    }
+
+    /**
+     * @Route("/single/projets/{id}", name="app_view_projets") 
+     */
+    public function singleProjets(MesProjets $projets): Response 
+    {
+        
+        $projets = $this->manager->getRepository(MesProjets::class)->find($projets);
+
+        return $this->render('mes_projets/singleProjets.html.twig', [
+            'projets' => $projets,
+             
+        ]);  
     
     }
 
--- src/Form/ProjetsType.php
+++ src/Form/ProjetsType.php
@@ -6,6 +6,7 @@ use App\Entity\MesProjets;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -16,7 +17,13 @@ class ProjetsType extends AbstractType
         $builder
             ->add('nom',TextType::class,['attr' => ['class' => 'form-control']])
             ->add('lien',TextType::class,['attr' => ['class' => 'form-control']])
-            ->add('images',TextType::class,['attr' => ['class' => 'form-control']])
+            ->add('images', FileType::class,[
+                'required'=>false,
+                'data_class'=>null,
+                'attr'=>[
+                    'class'=>'mt-3, mx-3'
+                ]
+            ])
             ->add('ajouter', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-success mt-3'  
